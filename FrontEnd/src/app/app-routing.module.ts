05d76db52b77c21eedcf96fb461e0d4f1eecfceb
Aibulat <aibulat122@yandex.ru>
2021-04-26 15:00:15 +0300

    Кое-какие доработки

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { UserGuard } from './User/user.guard';
 import { AddProductComponent } from './User/user-interface/add-product/add-product.component';
 import { UserInterfaceComponent } from './User/user-interface/user-interface.component';
 import { ProductComponentList } from './ProductComponents/Product-list/product.component';
-import { UserComponent } from './User/userRegistration/user.component';
+import { UserComponent } from './User/userRegistration/user.component'
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserauthorizationComponent } from './User/userauthorization/userauthorization.component';
