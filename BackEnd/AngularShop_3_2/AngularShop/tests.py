a470374e87317c85b8690b12d4745d3c05f3a1d2
Aibulat <aibulat122@yandex.ru>
2021-05-01 16:14:17 +0300

    From backup to git

--- BackEnd/AngularShop_3_2/AngularShop/tests.py
+++ BackEnd/AngularShop_3_2/AngularShop/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from models import AppPhone,UserTable,Cart
 # Create your tests here.
+from datetime import datetime
+
+class AppPhoneTestClass(TestCase):
+
+    def setUp(self)->None:
+        self.User=UserTable.objects.create(name='Test',surname='Test',login='TestLogin',
+                                           password='TestPass',email='Test@mail.ru',age='20',
+                                           registrationdate=datetime.now(),imgurl='',role='U')
+        self.Admin=UserTable.objects.create(name='TestAdmin',surname='TestAd',login='TestAdminLogin',
+                                           password='TestPass',email='TestAdmin@mail.ru',age='23',
+                                           registrationdate=datetime.now(),imgurl='',role='A')
